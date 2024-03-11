c00611e0e204dc074c6893caf34f07f13e1083a6
jocarsa <info@josevicentecarratala.com>
2024-03-11 10:08:47 +0100

    Tamaño y centrado en la pantalla

--- principal.py
+++ principal.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 
 bienvenida = tk.Tk()
-bienvenida.geometry("400x400")
 ventana = tk.Tk()
-ventana.geometry("1024x768")
+
+anchura = ventana.winfo_screenwidth()
+altura = ventana.winfo_screenheight()
+
+bienvenida.geometry(f"400x400+{int(anchura/2)-200}+{int(altura/2)-200}")
+ventana.geometry(f"1024x768+{int(anchura/2)-512}+{int(altura/2)-384}")
+
 ventana.mainloop()
 bienvenida.mainloop()
