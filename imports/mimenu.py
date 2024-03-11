b9131ecbc229b7d0e7978afb7aa9d50a7391348a
jocarsa <info@josevicentecarratala.com>
2024-03-11 10:53:02 +0100

    Menu abrir archivo

--- imports/mimenu.py
+++ imports/mimenu.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 
+def abrirArchivo():
+    print("Abro un archivo")
+
 def menu(ventana):
     menu = tk.Menu(ventana)
     ventana.config(menu=menu)
     archivo = tk.Menu(menu)
     menu.add_cascade(label="Archivo",menu=archivo)
-
+    
+    archivo.add_command(label="Abrir archivo",command=abrirArchivo)
+    
     menu.add_cascade(label="Editar",menu=archivo)
 
     menu.add_cascade(label="Colores",menu=archivo)
