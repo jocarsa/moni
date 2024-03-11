790020dfb9f27705a8553834d50aabdfeea17155
jocarsa <info@josevicentecarratala.com>
2024-03-11 11:04:01 +0100

    Distribucion principal

--- imports/ventana.py
+++ imports/ventana.py
@@ -36,7 +36,7 @@ def estilizarVentana(bienvenida,ventana):
     tk.Label(bienvenida,image=foto_bienvenida).pack()
 
     menu(ventana)
-
+    
     herramientas = tk.Frame(ventana)
     herramientas.grid(row=0,column=0)
 
@@ -51,6 +51,11 @@ def estilizarVentana(bienvenida,ventana):
     propiedades.grid(row=0,column=2)
 
     tk.Label(propiedades,text="Propiedades").pack()
+
+    ventana.rowconfigure(0,weight=100)
+    ventana.columnconfigure(0,weight=10)
+    ventana.columnconfigure(1,weight=80)
+    ventana.columnconfigure(2,weight=10)
 
     ventana.mainloop()
     bienvenida.mainloop()
