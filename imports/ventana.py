8e360db1234cdcdce1a6ea49e906b87db266e85e
jocarsa <info@josevicentecarratala.com>
2024-03-11 11:05:51 +0100

    Externalizar a archivos

--- imports/ventana.py
+++ imports/ventana.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageTk
 import tkinter as tk
 from ttkbootstrap import Style
 from imports.mimenu import *
+from imports.bloquesprincipal import *
 
 def estilizarVentana(bienvenida,ventana):
 
@@ -36,26 +37,8 @@ def estilizarVentana(bienvenida,ventana):
     tk.Label(bienvenida,image=foto_bienvenida).pack()
 
     menu(ventana)
+    bloques(ventana)
     
-    herramientas = tk.Frame(ventana)
-    herramientas.grid(row=0,column=0)
-
-    tk.Label(herramientas,text="Herramientas").pack()
-
-    principal = tk.Frame(ventana)
-    principal.grid(row=0,column=1)
-
-    tk.Label(principal,text="Principal").pack()
-
-    propiedades = tk.Frame(ventana)
-    propiedades.grid(row=0,column=2)
-
-    tk.Label(propiedades,text="Propiedades").pack()
-
-    ventana.rowconfigure(0,weight=100)
-    ventana.columnconfigure(0,weight=10)
-    ventana.columnconfigure(1,weight=80)
-    ventana.columnconfigure(2,weight=10)
 
     ventana.mainloop()
     bienvenida.mainloop()
